c36be6887cd0c9c32ca32f60d7bedc31a8f2b353
KavyamSachdeva <124245507+Kavyam172@users.noreply.github.com>
2024-01-30 11:53:36 +0530

    Time: 5 ms (34.61%) | Memory: 7.5 MB (7.39%) - LeetSync

--- 69-sqrtx/sqrtx.cpp
+++ 69-sqrtx/sqrtx.cpp
@@ -3,7 +3,31 @@
 class Solution {
 public:
     int mySqrt(int x) {
-        int result = sqrt(x);
+        // using sqrt() function
+        // int result = sqrt(x);
+
+        //using binary search
+
+        int result = -1;
+        int start = 0;
+        int end = x;
+        long int mid;
+        
+        while(start<=end){
+            mid = (start + end)/2;
+            if(mid*mid==x){
+                return mid;
+            }
+            else if(mid*mid<x){
+                result=mid;
+                start=mid+1;
+            }
+            else{
+                end=mid-1;
+            }
+            // mid = (start+end)/2;
+        }
+
         return result;
         
     }
