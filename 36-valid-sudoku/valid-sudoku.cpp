813c1af16edaaab7e3acd6efba7b52cd53064e6b
KavyamSachdeva <124245507+Kavyam172@users.noreply.github.com>
2024-01-31 11:18:04 +0530

    Time: 3 ms (99.93%) | Memory: 21.7 MB (25.23%) - LeetSync

---
+++ 36-valid-sudoku/valid-sudoku.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+
+
+    bool valid(vector<vector<char>>& board,int i,int j){
+        for(int k=0;k<9;k++){
+            if(board[i][j]==board[i][k] && k!=j){
+                return false;
+            }
+
+            if(board[i][j]==board[k][j] && k!=i){
+                return false;
+            }
+
+            if(board[i][j]==board[((i/3)*3)+(k/3)][((j/3)*3)+(k%3)] && ((i/3)*3)+(k/3)!=i && ((j/3)*3)+(k%3)!=j){
+                cout<<1;
+                return false;
+            }
+
+        }
+        return true;
+    }
+
+
+    bool isValidSudoku(vector<vector<char>>& board) {
+        for(int i=0;i<9;i++){
+            for(int j=0;j<9;j++){
+                if(board[i][j]!='.' && !valid(board,i,j)){
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+};
