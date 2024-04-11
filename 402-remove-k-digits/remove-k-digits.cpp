dc00fc784023901e87d9cb88467cab4e34716bee
KavyamSachdeva <124245507+Kavyam172@users.noreply.github.com>
2024-04-11 23:17:13 +0530

    Time: 15 ms (19.57%) | Memory: 10.3 MB (18.33%) - LeetSync

---
+++ 402-remove-k-digits/remove-k-digits.cpp
@@ -0,0 +1,53 @@
+class Solution {
+public:
+    string removeKdigits(string num, int k) {
+        string result="";
+        stack<char> st;
+        int n=0,i=1,zeros=0;
+        st.push(num[0]);
+        while(i<num.size()){
+            if(st.empty()){
+                st.push(num[i]);
+                i++;
+            }
+            if(num[i]>st.top()){
+                st.push(num[i]);
+            }
+            else if(num[i]<st.top()){
+                st.pop();
+                // st.push(num[i]);
+                n++;
+                if(n==k){
+                    break;
+                }
+                continue;
+            }
+            else{
+                st.push(num[i]);
+            }
+            i++;
+
+            
+        }
+        while(!st.empty() && n<k){
+            st.pop();
+            n++;
+        }
+        while(i<num.size()){
+            st.push(num[i]);
+            i++;
+        }
+        while(!st.empty()){
+            result+=st.top();
+            st.pop();
+        }
+        reverse(result.begin(),result.end());
+        while(result[0]=='0'){
+            result.erase(0,1);
+        }
+        if(result==""){
+            return "0";
+        }
+        return result;
+    }
+};
