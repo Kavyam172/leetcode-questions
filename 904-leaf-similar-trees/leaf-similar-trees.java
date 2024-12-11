1f61d734254e3ca047d8fcd5a4a4e8b6d3f92572
KavyamSachdeva <124245507+Kavyam172@users.noreply.github.com>
2024-12-11 11:37:56 +0530

    Time: 0 ms (100.00%) | Memory: 41.7 MB (19.27%) - LeetSync

---
+++ 904-leaf-similar-trees/leaf-similar-trees.java
@@ -0,0 +1,45 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public void dfs(TreeNode root,List<Integer> ans){
+        if(root==null){
+            return ;
+        }
+        if(root.left==null && root.right==null){
+            ans.add(root.val);
+            return;
+        }
+        dfs(root.left,ans);
+        dfs(root.right,ans);
+    }
+    public boolean leafSimilar(TreeNode root1, TreeNode root2) {
+        List<Integer> seq1 = new ArrayList<>();
+        List<Integer> seq2 = new ArrayList<>();
+        dfs(root1,seq1);
+        dfs(root2,seq2);
+        if(seq1.size()!=seq2.size()){
+            return false;
+        }
+        for(int i=0;i<seq1.size();i++){
+            int a = seq1.get(i);
+            int b = seq2.get(i);
+            if(a!=b){
+                return false;
+            }
+        }
+        return true;
+    }
+}
