550262b74af2125ab71257c70fe589527f612222
KavyamSachdeva <124245507+Kavyam172@users.noreply.github.com>
2025-01-24 10:16:15 +0530

    Time: 4 ms (99.02%) | Memory: 55.9 MB (15.73%) - LeetSync

--- 820-find-eventual-safe-states/find-eventual-safe-states.java
+++ 820-find-eventual-safe-states/find-eventual-safe-states.java
@@ -1,6 +1,12 @@
 class Solution {
-    public boolean dfs(int[][] graph,List<Integer> ans, int src, boolean[] visited){
-        if(ans.contains(src)){
+
+    // the nodes that do not end up in a cycle are safe nodes. So we need to check for cycles here.
+    // we start from one node then visit all its adjacent node to find a cycle.
+    // if cycle is found node is unsafe.
+    // we keep track of all nodes in 2 aspects. if node is visited and if node is safe.
+    // a node that is not safe and is visited means it contains a cycle. 
+    public boolean dfs(int[][] graph,boolean[] isSafe, int src, boolean[] visited){
+        if(isSafe[src]){
             return true;
         }
 
@@ -10,24 +16,29 @@ class Solution {
         visited[src] = true;
 
         for(int i:graph[src]){
-            if(!dfs(graph,ans,i,visited)){
+            if(!dfs(graph,isSafe,i,visited)){
                 return false;
             }
         }
 
-        ans.add(src);
+        isSafe[src] = true;
         return true;
 
     }
     public List<Integer> eventualSafeNodes(int[][] graph) {
         List<Integer> ans = new ArrayList<>();
+        boolean[] isSafe = new boolean[graph.length];
         boolean[] visited = new boolean[graph.length];
 
         for(int i=0;i<graph.length;i++){
-            dfs(graph,ans,i,visited);
+            dfs(graph,isSafe,i,visited);
         }
 
-        Collections.sort(ans);
+        for(int i=0;i<isSafe.length;i++){
+            if(isSafe[i]){
+                ans.add(i);
+            }
+        }
         return ans;
     }
 }
